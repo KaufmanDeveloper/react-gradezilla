5c1722c5e50108f907231e984ee4aaba1528ffd7
Michael Kaufman <mkaufman94@gmail.com>
2019-05-26 15:26:47 -0400

    feat: Import react router for routing to assignments and refactor css

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,27 +2,39 @@ import React, { Component } from "react";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-import "./home.css";
+import "../App.css";
+import { Redirect } from "react-router";
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false,
+      routeTo: ""
+    };
+  }
+
   render() {
+    if (this.state.redirect) {
+      return <Redirect push to={this.state.routeTo} />;
+    }
+
     return (
       <div>
         <Box component="span" m={1}>
           <Grid container className={"home"} spacing={2}>
             <Grid item xs={12}>
               <Grid container justify="center" spacing={2}>
-                <Card className="home-card">
+                <Card
+                  className="home-card"
+                  onClick={() => {
+                    this.setState({ redirect: true, routeTo: "/assignments" });
+                  }}
+                >
                   <CardActionArea>
-                    <CardMedia
-                      className={"card-media"}
-                      image="/static/images/cards/contemplative-reptile.jpg"
-                      title="Contemplative Reptile"
-                    />
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="h2">
                         Assignments
